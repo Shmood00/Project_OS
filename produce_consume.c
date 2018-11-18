bba6535f68d62ae29c4f283e81199205347bc322
ian <IanGerics>
2018-11-18 11:39:32 -0800

    Added Consumer Function

--- produce_consume.c
+++ produce_consume.c
@@ -5,11 +5,11 @@
 #include <semaphore.h>
 #include "buffer.h"
 
-pthread_mutex_t mutex;						
+pthread_mutex_t mutex;							
 sem_t full, empty;								
 buffer_item buffer[BUFFER_SIZE];				
 int counter = 0;								
-long long di = 10000000;						
+long long di = 1000000000;						
 pthread_t tid;       							
 pthread_attr_t attr; 							
 
@@ -32,7 +32,6 @@ int insert_item(buffer_item item) {
 		return -1;	
 } 
 
-
 void *producer(void *param) {
 	buffer_item item;
 
@@ -40,6 +39,7 @@ void *producer(void *param) {
 		sleep(rand()/di);
 		item = rand();
 
+		
 		sem_wait(&empty);
 		pthread_mutex_lock(&mutex);
 
@@ -48,24 +48,52 @@ void *producer(void *param) {
 		else 
 			printf("producer produced %d\n", item);		
 		
+		
 		pthread_mutex_unlock(&mutex);    
 		sem_post(&full);
 	  
    	}
 }
 
+void *consumer(void *param) {
+   	buffer_item item;
+
+   	while(1 != 0) {
+	    sleep(rand()/di);
+
+		
+      	sem_wait(&full);
+      	pthread_mutex_lock(&mutex);
+
+		if(remove_item(&item)) 
+			fprintf(stderr, "Consumer report error condition\n");
+		else 
+         	printf("consumer consumed %d\n", item);
+
+		
+		pthread_mutex_unlock(&mutex);		
+		sem_post(&empty);		
+   }
+}
+
 int main(int argc, char *argv[])
 {
 
+
 	int sleepTime = atoi(argv[1]);
-	int numProducers = atoi(argv[2]);	
+	int numProducers = atoi(argv[2]);
+	int numConsumers = atoi(argv[3]);	
 
 	initialize();
-	
+
 	int i;
 	for(i = 0; i < numProducers; i++) {
-      		pthread_create(&tid, &attr, producer, NULL);
+      pthread_create(&tid, &attr, consumer, NULL);
    	}
+
+	for (i = 0; i < numConsumers; i++) {
+		pthread_create(&tid, &attr, producer, NULL);
+	}
 
 	sleep(sleepTime);
 	return 0;
